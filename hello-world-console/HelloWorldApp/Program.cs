c84824309813b854c7c025e7e8bc4d551920ee57
Mihai Moldovan <mihai.moldovan@atos.net>
2025-10-10 15:00:59 +0200

    Initial commit GitHub Copilot Training

---
+++ hello-world-console/HelloWorldApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorldApp
+{
+    class Program
+    {
+        /// <summary>
+        /// The entry point of the application.
+        /// Generates equations for addition and subtraction and displays a message to the console.
+        /// </summary>
+        /// <param name="args">An array of command-line arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generate equations for addition and subtraction:");
+        }
+    }
+}
